6f5290cb70f28638d9e30561a538e41d712f86d4
kiishkishan <kishanth001@gmail.com>
2019-04-30 16:43:01 +0530

    defect table remodified

--- src/main/java/com/sgic/defecttracker/model/Defect.java
+++ src/main/java/com/sgic/defecttracker/model/Defect.java
@@ -1,5 +1,8 @@
 package com.sgic.defecttracker.model;
 
+import java.io.Serializable;
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,27 +10,82 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.validator.constraints.br.TituloEleitoral;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 @Table(name = "defects")
-public class Defect {
+public class Defect implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	
+	private String defect_id;
 	
 	private String module;
     
 	@Column(columnDefinition = "text")
 	private String description;
 
-	private String steps;
+	private String stepstorecreate;
 	private String severity;
 	private String priority;
-	private String defectType;
-	private String user;
+	private String defecttype;
 	private String status;
+	private String comments;
+	private String availablein;
+	private String enteredby;
+	private String assignedto;
+	private String fixedby;
+	
+	//dates
+	 @JsonFormat(pattern="yyyy-MM-dd")
+     private Date entereddate;
+	 
+	 @JsonFormat(pattern="yyyy-MM-dd")
+     private Date fixeddate;
 	
 	
+	public String getAvailablein() {
+		return availablein;
+	}
+	public void setAvailablein(String availablein) {
+		this.availablein = availablein;
+	}
+	public String getEnteredby() {
+		return enteredby;
+	}
+	public void setEnteredby(String enteredby) {
+		this.enteredby = enteredby;
+	}
+	public String getAssignedto() {
+		return assignedto;
+	}
+	public void setAssignedto(String assignedto) {
+		this.assignedto = assignedto;
+	}
+	public String getFixedby() {
+		return fixedby;
+	}
+	public void setFixedby(String fixedby) {
+		this.fixedby = fixedby;
+	}
+	public Date getEntereddate() {
+		return entereddate;
+	}
+	public void setEntereddate(Date entereddate) {
+		this.entereddate = entereddate;
+	}
+	public Date getFixeddate() {
+		return fixeddate;
+	}
+	public void setFixeddate(Date fixeddate) {
+		this.fixeddate = fixeddate;
+	}
 	public Long getId() {
 		return id;
 	}
@@ -39,12 +97,6 @@ public class Defect {
 	}
 	public void setDescription(String description) {
 		this.description = description;
-	}
-	public String getSteps() {
-		return steps;
-	}
-	public void setSteps(String steps) {
-		this.steps = steps;
 	}
 	public String getSeverity() {
 		return severity;
@@ -59,10 +111,10 @@ public class Defect {
 		this.priority = priority;
 	}
 	public String getDefectType() {
-		return defectType;
+		return defecttype;
 	}
 	public void setDefectType(String defectType) {
-		this.defectType = defectType;
+		this.defecttype = defectType;
 	}
 	public String getModule() {
 		return module;
@@ -70,17 +122,29 @@ public class Defect {
 	public void setModule(String module) {
 		this.module = module;
 	}
-	public String getUser() {
-		return user;
-	}
-	public void setUser(String user) {
-		this.user = user;
-	}
 	public String getStatus() {
 		return status;
 	}
 	public void setStatus(String status) {
 		this.status = status;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	public String getDefect_id() {
+		return defect_id;
+	}
+	public void setDefect_id(String defect_id) {
+		this.defect_id = defect_id;
+	}
+	public String getStepstorecreate() {
+		return stepstorecreate;
+	}
+	public void setStepstorecreate(String stepstorecreate) {
+		this.stepstorecreate = stepstorecreate;
 	}
 	
 	
--- src/main/java/com/sgic/defecttracker/repository/DeveloperRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sgic.defecttracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sgic.defecttracker.model.Developer;
-
-public interface DeveloperRepository extends JpaRepository<Developer,Long>{
-
-}
